4f748d2cdd975d25fbe787e1947fc09c62edaf25
apekshayadav85 <apekshayadav85@gmail.com>
2023-03-30 19:54:43 -0700

    Addition

---
+++ src/main/java/Basics/Addition.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b,c;
+		a=5;
+		b=10;
+		c=a+b;
+		
+		System.out.println("Addition is :" +c);
+	}
+
+}
