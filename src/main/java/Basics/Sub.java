a948fc4bb9ce3ef5d5f591da00fe26b6900ec669
apekshayadav85 <apekshayadav85@gmail.com>
2023-03-31 14:19:30 -0700

    Substraction

---
+++ src/main/java/Basics/Sub.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b,c;
+		a=20;
+		b=5;
+		c=a-b;
+		System.out.println(c);
+	}
+
+}
