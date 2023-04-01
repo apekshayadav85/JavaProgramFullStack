e34f9a8a4cce06b9535994c6b95bf9af369d4442
apekshayadav85 <apekshayadav85@gmail.com>
2023-03-31 17:28:03 -0700

    star

--- src/main/java/Basics/star1Pyramid.java
+++ src/main/java/Basics/star1Pyramid.java
@@ -5,18 +5,15 @@ public class star1Pyramid {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for(int i=0;i<5;i++)
-		{
-			//System.out.println("before j loop, i=" +i);
-			for(int k=i;k>5;k++)
-			{
-			  System.out.print(" ");
-			}			
-			for(int j=1;j<5;j++)
-			{
-			   System.out.print("* ");
+		for (int i = 0; i < 5; i++) {
+			// System.out.println("before j loop, i=" +i);
+			for (int k = i; k > 5; k++) {
+				System.out.print(" ");
 			}
-			//System.out.println("after j loop, i=" +i);
+			for (int j = 1; j < 5; j++) {
+				System.out.print("* ");
+			}
+			// System.out.println("after j loop, i=" +i);
 			System.out.println();
 		}
 		System.out.println();
--- src/main/java/Basics/starPyramid.java
+++ src/main/java/Basics/starPyramid.java
@@ -4,19 +4,16 @@ public class starPyramid {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		for(int i=0;i<5;i++)
-		{
-			//System.out.println("before j loop, i=" +i);
-			for(int k=i;k>0;k--)
-			{
-			  System.out.print(" ");
-			}			
-			for(int j=i;j<5;j++)
-			{
-			   System.out.print("* ");
+
+		for (int i = 0; i < 5; i++) {
+			// System.out.println("before j loop, i=" +i);
+			for (int k = i; k > 0; k--) {
+				System.out.print(" ");
 			}
-			//System.out.println("after j loop, i=" +i);
+			for (int j = i; j < 5; j++) {
+				System.out.print("* ");
+			}
+			// System.out.println("after j loop, i=" +i);
 			System.out.println();
 		}
 		System.out.println();
