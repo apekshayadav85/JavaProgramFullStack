d1cffa51512fe308ab3f72d3325cb24a25c1f63c
apekshayadav85 <apekshayadav85@gmail.com>
2023-03-31 22:13:14 -0700

    Triangle

---
+++ src/main/java/Basics/LeftTriPyramid.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class LeftTriPyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=i;j>0;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/main/java/Basics/RightTri.java
@@ -0,0 +1,22 @@
+package Basics;
+
+public class RightTri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++)
+		{
+			for(int k=5-i;k>0;k--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i;j>0;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
