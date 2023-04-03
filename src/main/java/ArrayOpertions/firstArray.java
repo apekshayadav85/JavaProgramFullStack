41b8e1c527fb686e2c6b335ddeefc330e3d9af8f
apekshayadav85 <apekshayadav85@gmail.com>
2023-04-03 15:16:12 -0700

    Array

---
+++ src/main/java/ArrayOpertions/firstArray.java
@@ -0,0 +1,10 @@
+package ArrayOpertions;
+
+public class firstArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/Basics/StringOpertions.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class StringOpertions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
